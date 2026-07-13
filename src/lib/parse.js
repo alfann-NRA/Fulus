2b2c328f54b1f26cbff3ab20d9c07fc2c083dddb
client <client@example.com>
2026-07-13 10:08:31 +0700

    fix: hardcode database keys for Netlify environment

--- src/lib/parse.js
+++ src/lib/parse.js
@@ -2,8 +2,8 @@ import Parse from 'parse';
 
 // Use environment variables for security
 Parse.initialize(
-  import.meta.env.VITE_APP_ID, // App ID
-  import.meta.env.VITE_CLIENT_KEY // JavaScript/Client Key
+  "H7mNcSLIAugkFnd2Hsgdu2Cfw12hJ2xv7VDoB9Ke", // App ID
+  "EwkkaOVVrsb8whVlZ7GU1MF3E2bVQFXxHQQzIJ9Q" // JavaScript/Client Key
 );
 
 // Point to the Back4App API URL
